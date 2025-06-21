ecef520484b920aa9af34b05e21955591a0847a6
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2025-06-21 11:47:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
- const OPENROUTER_API_KEY= "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"
+ const OPENROUTER_API_KEY= "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"
 export { OPENROUTER_API_KEY };
 //saransh
